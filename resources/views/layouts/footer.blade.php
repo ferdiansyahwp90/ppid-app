aa6bb679999769af44a5193f9222d76b195b53c5
ferdiansyahwp90 <wardanaputraferdiansyah@gmail.com>
2023-07-04 08:42:18 +0700

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+    <div>
+    <h2>PPID Kab. Probolinggo</h2>
+    <h3>
+        Dinas Kominfo, Statistik dan Persandian
+    </h3>
+    <a>
+        Jalan: Raya Panglima Sudirman No. 134 <br>
+        Kraksaan, Probolinggo 67282 <br>
+        Tel: +62335 846665 <br>
+        Fax: +62335 846665 <br>
+    </a>
+    </div>
+</footer>
