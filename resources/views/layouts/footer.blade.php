8c5ec3bf587808828356277a4228079517e7265d
ferdiansyahwp90 <wardanaputraferdiansyah@gmail.com>
2023-07-04 14:38:49 +0700

    add bootstrap

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,14 +1,39 @@
-<footer class="footer">
-    <div>
-    <h2>PPID Kab. Probolinggo</h2>
-    <h3>
-        Dinas Kominfo, Statistik dan Persandian
-    </h3>
-    <a>
-        Jalan: Raya Panglima Sudirman No. 134 <br>
-        Kraksaan, Probolinggo 67282 <br>
-        Tel: +62335 846665 <br>
-        Fax: +62335 846665 <br>
-    </a>
+<!-- ======= Footer ======= -->
+<footer id="footer">
+
+
+<div class="footer-top">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-lg-3 col-md-6 footer-contact">
+        <h3>PPID Kabupaten Probolinggo</h3> 
+        <h4>Dinas Kominfo, Statistik dan Persandian</h4>
+        <p>
+          Jalan: Raya Panglima Sudirman No. 134 <br>
+          Kraksaan, Probolinggo 67282 <br>
+          Tel: +62335 846665 <br>
+          Fax: +62335 846665 <br><br>
+          <strong>Phone:</strong> +62 335 846 665<br>
+          <strong>Fax:</strong> +62 335 846 665<br>
+          <strong>Email:</strong> info@example.com<br>
+        </p>
+      </div>
+
     </div>
-</footer>
+  </div>
+</div>
+
+<div class="container footer-bottom clearfix">
+  <div class="copyright">
+    &copy; Copyright <strong><span>Dinas Komunikasi dan Informatika.</span></strong>. All Rights Reserved
+  </div>
+  <div class="credits">
+    <!-- All the links in the footer should remain intact. -->
+    <!-- You can delete the links only if you purchased the pro version. -->
+    <!-- Licensing information: https://bootstrapmade.com/license/ -->
+    <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/ -->
+    <!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> -->
+  </div>
+</div>
+</footer><!-- End Footer -->
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,14 +1,60 @@
-<footer class="header">
-    <div>
-    <h2>PPID Kab. Probolinggo</h2>
-    <h3>
-        Dinas Kominfo, Statistik dan Persandian
-    </h3>
-    <a>
-        Jalan: Raya Panglima Sudirman No. 134 <br>
-        Kraksaan, Probolinggo 67282 <br>
-        Tel: +62335 846665 <br>
-        Fax: +62335 846665 <br>
-    </a>
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="/assets/img/ppid.png">PPID</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+            <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+            <li class="dropdown"><a href="#"><span>Profil</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+                  <li><a href="#">Seputar PPID</a></li>
+                  <li><a href="#">Struktur Organisasi</a></li>
+                  <li><a href="#">Tugas dan Fungsi PPID</a></li>
+                  <li><a href="#">Visi dan Misi PPID</a></li>
+                  <li><a href="#">Maklumat Pelayanan Informasi Publik</a></li>
+                  <li><a href="#">SOP PPID</a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="#"><span>Regulasi</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+                  <li><a href="#">PERGUB JATIM NO 8 TAHUN 2018</a></li>
+                  <li><a href="#">PERMENDAGRI NO 3 TAHUN 2017</a></li>
+                  <li><a href="#">PERATURAN PEMERINTAH NO 61 TAHUN 2010</a></li>
+                  <li><a href="#">PERKI NO 1 TAHUN 2013</a></li>
+                  <li><a href="#">UNDANG-UNDANG 43 TAHUN 2009</a></li>
+                  <li><a href="#">UNDANG-UNDANG 14 TAHUN 2008</a></li>
+                  <li><a href="#">PERKI NO 1 TAHUN 2021</a></li>
+                  <li><a href="#">SK PPID</a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="#"><span>Layanan</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+                  <li><a href="#">Layanan Informasi Daring</a></li>
+                  <li><a href="#">Permohonan Informasi Datang Langsung</a></li>
+                  <li><a href="#">Mekanisme/Proses</a></li>
+                  <li><a href="#">Pengajuan Keberatan</a></li>
+                  <li><a href="#">Prosedur Penyelesaian Sengketa</a></li>
+                  <li><a href="#">Laporan Akses Informasi Publik</a></li>
+                  <li><a href="#">Laporan Pelayanan Informasi Publik</a></li>
+            </ul>
+        </li>
+        <li><a class="nav-link scrollto" href="#">Data</a></li>
+        <li class="dropdown"><a href="#"><span>Kegiatan</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+                  <li><a href="#">Galeri</a></li>
+                  <li><a href="#">Berita</a></li>
+            </ul>
+        </li>
+        <li><a class="nav-link scrollto" href="#contact">Kontak</a></li>
+        <li><a class="getstarted scrollto" href="#about">Formulir</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
     </div>
-</footer>
+</header> 
+<!-- End Header -->
