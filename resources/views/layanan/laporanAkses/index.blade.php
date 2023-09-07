59694a0e59cfd0b133f3f454f346ae10d05a4b35
ferdiansyahwp90 <wardanaputraferdiansyah@gmail.com>
2023-09-07 09:50:33 +0700

    Dashboard-Layanan

--- resources/views/layanan/laporanAkses/index.blade.php
+++ resources/views/layanan/laporanAkses/index.blade.php
@@ -1,13 +1,11 @@
 @extends('layouts.firstpage')
 @section('content')
 <div class="container">
-<div class="text-center">
-    <h2>Laporan Akses Informasi Publik</h2>
-</div>
-    <ol>
-        <li>Laporan Akses Informasi Publikh Tahun 2018 |<a href="">Download</a> </li> 
-        <li>Laporan Akses Informasi Publik Tahun 2020 |<a href="">Download</a> </li>
-        <li>Laporan Akses Informasi Publik Tahun 2022 | <a href="">Download</a> </li>
-       </ol>
+    <div class="text-center">
+        <h2>Laporan Akses Informasi Publik</h2>
+    </div>
+    @foreach ($laporanAkses as $item)
+        <td>{!! $item->nama !!}</td>
+    @endforeach
 </div>
 @endsection
